04a410ecf2e6db81d210b53b1bcd2ebbdb76f03f
Pulkit <pulkit_mac@Pulkits-MacBook-Air.local>
2025-04-19 14:13:17 +0900

    Store expenses in a list

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+expenses = []
+
 def add_expense(amount, category):
-    print((f"Added expense: {amount} in category: {category}"))
+    expense = {"amount": amount, "category": category}
+    expenses.append(expense)
+    print(f'Added expense: {amount} in category: {category}')
+
+def show_expenses():
+    for expense in expenses:
+        print(f'{expense["category"]}: {expense["amount"]}')
 
 def main():
     add_expense(2000, "Groceries")
     add_expense(220, "Transportation")
+
+    print('\nAll Expenses:')
+    show_expenses()
 
 if __name__ == "__main__":
     main()
