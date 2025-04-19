85ad7d7e1017873f3ae7ad7d09dbf3eaa9760c2e
Pulkit <pulkit_mac@Pulkits-MacBook-Air.local>
2025-04-19 14:26:45 +0900

    Allow user input for expensesUpdated
    Allow user to input the expenses

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 expenses = []
 
+
 def add_expense(amount, category):
     expense = {"amount": amount, "category": category}
     expenses.append(expense)
     print(f'Added expense: {amount} in category: {category}')
 
+
 def show_expenses():
+    print('\nYour Expenses:')
     for expense in expenses:
         print(f'{expense["category"]}: {expense["amount"]}')
 
-def main():
-    add_expense(2000, "Groceries")
-    add_expense(220, "Transportation")
 
-    print('\nAll Expenses:')
-    show_expenses()
+def main():
+    while True:
+        print("1. Add Expense")
+        print("2. Show Expenses")
+        print("3. Exit")
+        choice = int(input("Enter the category: "))
+
+        if choice == 1:
+            amount = float(input("Enter the amount: "))
+            category = input("Enter the category: ")
+            add_expense(amount, category)
+        elif choice == 2:
+            show_expenses()
+        elif choice == 3:
+            break
+        else:
+            print("Invalid choice, please try again.")
+
 
 if __name__ == "__main__":
     main()
